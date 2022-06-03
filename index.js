cbd19b59cb1b3479107114345bd7a93334ec6ca8
Kristianto <kristianto@nexsoft.co.id>
2022-06-03 20:29:21 +0700

    Tantangan 3 JS Kristianto

--- index.js
+++ index.js
@@ -21,10 +21,25 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (belanjaan) => {
+  let items = [];
+  
+  for(let i=0; i<belanjaan.length; i++){
+    items.push('- '+belanjaan[i].nama+ ' x '+ belanjaan[i].kuantitas);
+  }
+
+  return items;
+}
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (belanjaan) => {
+  let totalPrice = 0;
+  for(let i=0; i<belanjaan.length; i++){
+      totalPrice += parseInt(belanjaan[i].harga) * parseInt(belanjaan[i].kuantitas);
+  } 
+  
+  return totalPrice;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
@@ -33,7 +48,7 @@ const main = () => {
   console.log("Belanjaan Anda adalah:");
   console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
   console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
+    "\nTotal Belanjaan Anda adalah " + totalBelanjaan?.(dataBelanjaan)
   );
 };
 
